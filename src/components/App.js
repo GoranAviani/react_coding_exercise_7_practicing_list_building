efff9b2a16e22e11eb04cd1f73f016d81a2013c7
GoranAviani <goranaviani@gmail.com>
2022-04-04 14:04:29 +0200

    react_coding_exercise_7_practicing_list_building

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+const users = [
+  { id: 1, name: 'Leanne Graham' },
+  { id: 2, name: 'Ervin Howell' },
+  { id: 3, name: 'Clementine Bauch' },
+  { id: 4, name: 'Patricia Lebsack' }
+];
+
+
 class App extends React.Component {
 
     render() {
         return (
-            <div>
-                App1
-            </div>
+            <ul>
+            </ul>
         )
     }
 }
